fda3bd46dde71afeca51b108ed5b98750dafeeee
Grandest Nothing <rodolforodex63@gmail.com>
2022-11-22 01:41:29 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+<h1 align="center"> RICKETY WRECKED </h1>
+
+<h3 align="center">
+  <a href="https://pokedex-plum-three.vercel.app">Acesse Meu App</a>
+</h3>
+
+<p align="center">
+  <img alt="rick" src="./img/screenshot.png" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML e CSS
+- JavaScript
+- Git e GitHub
+
+## 💻 Projeto
+
+Criar pagina onde os olhos do personagem seguem o ponteiro do mouse.
---
+++ script.js
@@ -0,0 +1,24 @@
+const anchor = document.getElementById("anchor");
+const rekt = anchor.getBoundingClientRect();
+const anchorX = rekt.left + rekt.width / 2;
+const anchorY = rekt.top + rekt.height / 2;
+
+document.addEventListener("mousemove", (e) => {
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+
+  const angleDeg = angle(mouseX, mouseY, anchorX, anchorY);
+  const eyes = document.querySelectorAll(".eye");
+  eyes.forEach((eye) => {
+    eye.style.transform = `rotate(${angleDeg}deg)`;
+    anchor.style.filter = `hue-rotate(${angleDeg}deg)`;
+  });
+});
+
+function angle(cx, cy, ex, ey) {
+  const dy = ey - cy;
+  const dx = ex - cx;
+  const rad = Math.atan2(dy, dx); //range -PI, PI
+  const deg = (rad * 180) / Math.PI;
+  return deg;
+}
